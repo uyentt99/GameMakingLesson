0659095b2b883de422f7ba341e90127dd344a654
uyentt99 <tranuyen0302@gmail.com>
2021-03-23 08:35:37 +0700

    lesson 5, 6

---
+++ Jura/TrainingFramework/src/GameStates/GSPlay.cpp
@@ -0,0 +1,107 @@
+#include "GSPlay.h"
+#include "Shaders.h"
+#include "Texture.h"
+#include "Models.h"
+#include "Camera.h"
+#include "Font.h"
+#include "Sprite2D.h"
+#include "Sprite3D.h"
+#include "Text.h"
+
+extern int screenWidth; //need get on Graphic engine
+extern int screenHeight; //need get on Graphic engine
+
+GSPlay::GSPlay()
+{
+}
+
+
+GSPlay::~GSPlay()
+{
+
+}
+
+
+void GSPlay::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("bg_run");
+
+	//BackGround
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_BackGround = std::make_shared<Sprite2D>(model, shader, texture);
+	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
+	m_BackGround->SetSize(screenWidth, screenHeight);
+
+	//back button
+	texture = ResourceManagers::GetInstance()->GetTexture("buttonB");
+	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(20, 20);
+	button->SetSize(42, 34);
+	button->SetOnClick([]() {
+		//GameStateMachine::GetInstance()->PopState();
+		GameStateMachine::GetInstance()->ChangeState(StateTypes::STATE_Menu);
+		});
+	m_listButton.push_back(button);
+
+
+	//text game title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("arialbd");
+	m_score = std::make_shared< Text>(shader, font, "score: 10", TEXT_COLOR::RED, 1.0);
+	m_score->Set2DPosition(Vector2(screenWidth-150, 30));
+}
+
+void GSPlay::Exit()
+{
+
+}
+
+
+void GSPlay::Pause()
+{
+
+}
+
+void GSPlay::Resume()
+{
+
+}
+
+
+void GSPlay::HandleEvents()
+{
+
+}
+
+void GSPlay::HandleKeyEvents(int key, bool bIsPressed)
+{
+	
+}
+
+void GSPlay::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto it : m_listButton)
+	{
+		(it)->HandleTouchEvents(x, y, bIsPressed);
+		if ((it)->IsHandle()) break;
+	}
+}
+
+void GSPlay::Update(float deltaTime)
+{
+}
+
+void GSPlay::Draw()
+{
+	m_BackGround->Draw();
+	m_score->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+}
+
+void GSPlay::SetNewPostionForBullet()
+{
+}
---
+++ Jura/TrainingFramework/src/GameStates/GSSetting.cpp
@@ -0,0 +1,112 @@
+#include "GSSetting.h"
+
+extern int screenWidth; //need get on Graphic engine
+extern int screenHeight; //need get on Graphic engine
+
+GSSetting::GSSetting()
+{
+
+}
+
+
+GSSetting::~GSSetting()
+{
+}
+
+
+
+void GSSetting::Init()
+{
+	auto model = ResourceManagers::GetInstance()->GetModel("Sprite2D");
+	auto texture = ResourceManagers::GetInstance()->GetTexture("run");
+
+	//BackGround
+	auto shader = ResourceManagers::GetInstance()->GetShader("TextureShader");
+	m_BackGround = std::make_shared<Sprite2D>(model, shader, texture);
+	m_BackGround->Set2DPosition(screenWidth / 2, screenHeight / 2);
+	m_BackGround->SetSize(screenWidth, screenHeight);
+
+	//back button
+	texture = ResourceManagers::GetInstance()->GetTexture("buttonBack");
+	std::shared_ptr<GameButton> button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(screenWidth / 2, 180);
+	button->SetSize(200, 50);
+	button->SetOnClick([]() {
+		GameStateMachine::GetInstance()->ChangeState(StateTypes::STATE_Play);
+		});
+	m_listButton.push_back(button);
+
+
+	//exit button
+	texture = ResourceManagers::GetInstance()->GetTexture("buttonE");
+	button = std::make_shared<GameButton>(model, shader, texture);
+	button->Set2DPosition(screenWidth / 2, 375);
+	button->SetSize(200, 50);
+	button->SetOnClick([]() {
+		exit(0);
+		});
+	m_listButton.push_back(button);
+
+	//text game title
+	shader = ResourceManagers::GetInstance()->GetShader("TextShader");
+	std::shared_ptr<Font> font = ResourceManagers::GetInstance()->GetFont("Algerian Regular");
+	m_Text_gameName = std::make_shared< Text>(shader, font, "JURA RUN", TEXT_COLOR::RED, 2.0);
+	m_Text_gameName->Set2DPosition(Vector2(screenWidth / 2 - 110, 120));
+
+
+
+}
+
+void GSSetting::Exit()
+{
+}
+
+
+void GSSetting::Pause()
+{
+
+}
+
+void GSSetting::Resume()
+{
+
+}
+
+
+void GSSetting::HandleEvents()
+{
+
+}
+
+void GSSetting::HandleKeyEvents(int key, bool bIsPressed)
+{
+
+}
+
+void GSSetting::HandleTouchEvents(int x, int y, bool bIsPressed)
+{
+	for (auto it : m_listButton)
+	{
+		(it)->HandleTouchEvents(x, y, bIsPressed);
+		if ((it)->IsHandle()) break;
+	}
+}
+
+void GSSetting::Update(float deltaTime)
+{
+	m_BackGround->Update(deltaTime);
+	for (auto it : m_listButton)
+	{
+		it->Update(deltaTime);
+	}
+}
+
+void GSSetting::Draw()
+{
+	m_BackGround->Draw();
+	for (auto it : m_listButton)
+	{
+		it->Draw();
+	}
+	m_Text_gameName->Draw();
+}
